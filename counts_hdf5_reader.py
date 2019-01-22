7a8a6a87cf584b13a4ede9899c8d589752c74dc9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-22 10:57:43 -0500

    counts has a simple structure

--- counts_hdf5_reader.py
+++ counts_hdf5_reader.py
@@ -1,3 +1,30 @@
+from h5py import File
+
+
 class CountsHdf5Reader:
     def __init__(self, filename):
-        pass
+        self.data = File(filename, 'r')
+
+    def keys(self):
+        '''
+        # >>> path = 'fixtures/mRNA_coords_raw_counting.hdf5'
+        # >>> reader = CountsHdf5Reader(path)
+        # >>> len(reader.keys())
+        # 39
+        # >>> sorted(list(reader.keys()))[:3]
+        # ['Acta2_Hybridization5', 'Aldoc_Hybridization1', 'Anln_Hybridization9']
+
+        '''
+        return self.data.keys()
+
+    def __getitem__(self, key):
+        '''
+        # >>> path = 'fixtures/mRNA_coords_raw_counting.hdf5'
+        # >>> reader = CountsHdf5Reader(path)
+        # >>> reader['Acta2_Hybridization5'].shape
+        # (13052, 2)
+        # >>> reader['Acta2_Hybridization5'][0]
+        # array([18215., 20052.])
+
+        '''
+        return self.data[key]
