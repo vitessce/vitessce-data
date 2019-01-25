ed8ab10381ad3dbcb35dd330ba455fb21f67bdb6
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-25 15:15:02 -0500

    Export cell data Polygon representation could be much more efficient, Gene dict for now feels like less of an issue

--- cell_reader.py
+++ cell_reader.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 import argparse
+from pickle import load
 
 from loom_reader import LoomReader
-from segmentation_pickle_reader import SegPickleReader
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Create JSON with cell metadata and segmentation.')
     parser.add_argument('--pickle', required=True,
         help='Pickle file with cell segmentation data')
@@ -15,4 +15,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     loom = LoomReader(args.loom)
-    pickle = SegPickleReader(args.pickle)
+    with open(args.pickle, 'rb') as f:
+        segmentation = load(f)
+
+    metadata = loom.data()
+
+    for cell_id, poly in segmentation.items():
+        if cell_id in metadata:
+            metadata[cell_id]['poly'] = poly.tolist()
+
+    print(json.dumps(metadata, indent=1))
