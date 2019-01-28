94ebcb1d59bde0632526a24a2dff3ecded6f36ad
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-28 15:01:10 -0500

    make the polygons optional

--- cell_reader.py
+++ cell_reader.py
@@ -8,20 +8,19 @@ from loom_reader import LoomReader
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create JSON with cell metadata and segmentation.')
-    parser.add_argument('--pickle', required=True,
-        help='Pickle file with cell segmentation data')
     parser.add_argument('--loom', required=True,
         help='Loom file with cell metadata')
+    parser.add_argument('--pickle',
+        help='Pickle file with cell segmentation data')
     args = parser.parse_args()
 
-    loom = LoomReader(args.loom)
-    with open(args.pickle, 'rb') as f:
-        segmentation = load(f)
+    metadata = LoomReader(args.loom).data()
 
-    metadata = loom.data()
-
-    for cell_id, poly in segmentation.items():
-        if cell_id in metadata:
-            metadata[cell_id]['poly'] = poly.tolist()
+    if args.pickle:
+        with open(args.pickle, 'rb') as f:
+            segmentation = load(f)
+        for cell_id, poly in segmentation.items():
+            if cell_id in metadata:
+                metadata[cell_id]['poly'] = poly.tolist()
 
     print(json.dumps(metadata, indent=1))
--- loom_reader.py
+++ loom_reader.py
@@ -20,14 +20,17 @@ class LoomReader:
             self.ds.ca['_tSNE_1'],
             self.ds.ca['_tSNE_2'],
             self.ds.ca['ClusterID'],
-            self.ds.ca['ClusterName']
+            self.ds.ca['ClusterName'],
+            self.ds.ca['X'],
+            self.ds.ca['Y']
         )
-        for (valid, cell_id, tsne1, tsne2, cluster_id, cluster_name) in data_zip:
+        for (valid, cell_id, tsne1, tsne2, cluster_id, cluster_name, x, y) in data_zip:
             if valid:
                 cells[cell_id] = {
                     'tsne': [tsne1, tsne2],
                     'cluster': cluster_name,
-                    'genes': self.by_cell(cell_id)
+                    'genes': self.by_cell(cell_id),
+                    'xy': [x, y]
                 }
         return cells
 
