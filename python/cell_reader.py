38071ad63ab1c799d2142003d5a853a0724670a6
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-05 13:20:19 -0500

    bounding octagon

--- python/cell_reader.py
+++ python/cell_reader.py
@@ -8,6 +8,59 @@ import numpy as np
 
 from loom_reader import LoomReader
 
+
+def octagon(poly):
+    '''
+    Returns a bounding octagon.
+    >>> square = np.array([[0,0], [0,1], [1,1], [1,0]])
+    >>> octagon(square)
+    [[0, 0], [0, 1], [0, 1], [1, 1], [1, 1], [1, 0], [1, 0], [0, 0]]
+
+    >>> triangle = np.array([[1,0], [0,2], [2,3]])
+    >>> octagon(triangle)
+    [[0, 1], [0, 2], [1, 3], [2, 3], [2, 3], [2, 1], [1, 0], [1, 0]]
+
+    >>> type(octagon(triangle)[0][0])
+    <class 'int'>
+    '''
+    # SciPy has ConvexHull, but it segfaulted on me: perhaps
+    #   https://github.com/scipy/scipy/issues/9751
+    # ... and even if I fixed it locally,
+    # not sure we want that fragility.
+    #
+    # Also: The goal is really just to get a simplified shape...
+    # A convex hull is too precise in some ways,
+    # while in others it falls short, ie, concavities.
+    #
+    # I kind-of like the obvious artificiality of an octagon.
+
+    # Was unsigned, and substraction causes underflow.
+    poly_as_int = poly.astype('int')
+    min_x = int(np.min(poly_as_int[:, [0]]))
+    max_x = int(np.max(poly_as_int[:, [0]]))
+    min_y = int(np.min(poly_as_int[:, [1]]))
+    max_y = int(np.max(poly_as_int[:, [1]]))
+
+    summed = np.sum(poly_as_int, axis=1)
+    diffed = np.diff(poly_as_int, axis=1)
+
+    min_sum = int(np.min(summed))
+    max_sum = int(np.max(summed))
+    min_diff = int(np.min(diffed))
+    max_diff = int(np.max(diffed))
+
+    return [
+        [min_x, min_sum - min_x],
+        [min_x, max_diff + min_x],  # ^ Left
+        [max_y - max_diff, max_y],
+        [max_sum - max_y, max_y],  # ^ Botton
+        [max_x, max_sum - max_x],
+        [max_x, min_diff + max_x],  # ^ Right
+        [min_y - min_diff, min_y],
+        [min_sum - min_y, min_y]  # ^ Top
+    ]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Create JSON with cell metadata and, '
@@ -31,37 +84,6 @@ if __name__ == '__main__':
             segmentation = pickle.load(f)
         for cell_id, poly in segmentation.items():
             if cell_id in metadata:
-                # SciPy has ConvexHull, but it segfaulted on me: perhaps
-                #   https://github.com/scipy/scipy/issues/9751
-                # ... and even if I fixed it locally,
-                # not sure we want that fragility.
-                #
-                # Also: The goal is really just to get a simplified shape...
-                # A convex hull is too precise in some ways,
-                # while in others it falls short, ie, concavities.
-                #
-                # I kind-of like the obvious artificiality of an octagon.
-
-                # Was unsigned, and substraction causes underflow.
-                poly_as_int = poly.astype('int')
-                min_x = np.min(poly_as_int[:, [0]])
-                max_x = np.max(poly_as_int[:, [0]])
-                min_y = np.min(poly_as_int[:, [1]])
-                max_y = np.max(poly_as_int[:, [1]])
-
-                summed = np.sum(poly_as_int, axis=1)
-                diffed = np.diff(poly_as_int, axis=1)
-
-                min_sum = np.min(summed)
-                max_sum = np.max(summed)
-                min_diff = np.min(diffed)
-                max_diff = np.max(diffed)
-
-                metadata[cell_id]['poly'] = [
-                    [min_x, min_y],
-                    [min_x, max_y],
-                    [max_x, max_y],
-                    [max_x, min_y]
-                ]
+                metadata[cell_id]['poly'] = octagon(poly)
 
     print(json.dumps(metadata, indent=1))
