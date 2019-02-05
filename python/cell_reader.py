8b069abdb2a9bda7ac38adad0e9d8004bb3c646c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-05 11:58:33 -0500

    checkpoint: do more math

--- python/cell_reader.py
+++ python/cell_reader.py
@@ -34,20 +34,34 @@ if __name__ == '__main__':
                 # SciPy has ConvexHull, but it segfaulted on me: perhaps
                 #   https://github.com/scipy/scipy/issues/9751
                 # ... and even if I fixed it locally,
-                # not sure I'd want that fragility.
+                # not sure we want that fragility.
                 #
                 # Also: The goal is really just to get a simplified shape...
                 # A convex hull is too precise in some ways,
                 # while in others it falls short, ie, concavities.
-                w = int(np.min(poly[:, [0]]))
-                e = int(np.max(poly[:, [0]]))
-                n = int(np.min(poly[:, [1]]))
-                s = int(np.max(poly[:, [1]]))
+                #
+                # I kind-of like the obvious artificiality of an octagon.
+
+                # Was unsigned, and substraction causes underflow.
+                poly_as_int = poly.astype('int')
+                min_x = np.min(poly_as_int[:, [0]])
+                max_x = np.max(poly_as_int[:, [0]])
+                min_y = np.min(poly_as_int[:, [1]])
+                max_y = np.max(poly_as_int[:, [1]])
+
+                summed = np.sum(poly_as_int, axis=1)
+                diffed = np.diff(poly_as_int, axis=1)
+
+                min_sum = np.min(summed)
+                max_sum = np.max(summed)
+                min_diff = np.min(diffed)
+                max_diff = np.max(diffed)
+
                 metadata[cell_id]['poly'] = [
-                    [w, n],
-                    [w, s],
-                    [e, s],
-                    [e, n]
+                    [min_x, min_y],
+                    [min_x, max_y],
+                    [max_x, max_y],
+                    [max_x, min_y]
                 ]
 
     print(json.dumps(metadata, indent=1))
