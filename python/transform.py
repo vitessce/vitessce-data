5692496f453eb2f08d12ab20cd5a591cbb7dab4f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-20 13:46:39 -0500

    Preserve aspect ratio

--- python/transform.py
+++ python/transform.py
@@ -8,11 +8,15 @@ def get_transform(metadata):
     domain = 2000
     # Limited by raster-based picking in deck.gl:
     # https://github.com/uber/deck.gl/issues/2658#issuecomment-463293063
+    scale = domain / min(
+        max_xy[0] - min_xy[0],
+        max_xy[1] - min_xy[1]
+    )
     return {
         'x_shift': - (max_xy[0] + min_xy[0]) / 2,
         'y_shift': - (max_xy[1] + min_xy[1]) / 2,
-        'x_scale': domain / (max_xy[0] - min_xy[0]),
-        'y_scale': domain / (max_xy[1] - min_xy[1])
+        'x_scale': scale,
+        'y_scale': scale
     }
 
 
