5e8bfff9145ac0141d6765c84d5409864a201667
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-20 12:12:11 -0500

    fix the math!

--- python/counts_hdf5_reader.py
+++ python/counts_hdf5_reader.py
@@ -39,8 +39,8 @@ class CountsHdf5Reader:
 def apply_transform(transform, xy):
     # TODO: Rounding should be based on precision of input.
     return [
-        round((xy[0] - transform['x_shift']) * transform['x_scale'], 2),
-        round((xy[1] - transform['y_shift']) * transform['y_scale'], 2)
+        round((xy[0] + transform['x_shift']) * transform['x_scale'], 2),
+        round((xy[1] + transform['y_shift']) * transform['y_scale'], 2)
     ]
 
 
