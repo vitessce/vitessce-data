7062bee241a2940e1f2ee560fa6cda57ea0d734d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-16 15:48:00 -0400

    Fix image dimensions, I think

--- python/img_hdf5_reader.py
+++ python/img_hdf5_reader.py
@@ -84,8 +84,10 @@ class ImgHdf5Reader:
         image.save(png_path)
 
         return {
-            'width': hack.shape[0],
-            'height': hack.shape[1],
+            'x': 0,
+            'y': 0,
+            'width': hack.shape[0] * step,
+            'height': hack.shape[1] * step,
             'href': 'https://s3.amazonaws.com/{}/{}'.format(
                 s3_target, png_basename
             )
