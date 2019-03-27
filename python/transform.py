ddc0da44848279aadebd73e0f8c277f2b8391aee
Chuck McCallum <mccalluc@users.noreply.github.com>
2019-03-27 12:40:10 -0400

    Remove todos [skip ci] (#22)

--- python/transform.py
+++ python/transform.py
@@ -21,7 +21,6 @@ def get_transform(metadata):
 
 
 def apply_transform(transform, xy):
-    # TODO: Rounding should be based on precision of input.
     return [
         round((xy[0] + transform['x_shift']) * transform['x_scale'], 2),
         round((xy[1] + transform['y_shift']) * transform['y_scale'], 2)
