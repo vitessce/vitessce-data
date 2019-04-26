f04a0260419cd67dff897cbbe991347515914623
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-26 17:42:10 -0400

    I think this would work... but the tiling takes forever!

--- linnarsson-osmfish.sh
+++ linnarsson-osmfish.sh
@@ -33,7 +33,7 @@ main() {
     # Exclude the *HUGE* PNGs in the base directory:
     # The tiles for S3 are in subdirectories;
     # We keep the PNGs around because it takes a long time to generate them.
-    aws s3 cp --recursive "$OUTPUT" --exclude "$OUTPUT"/*.png s3://"$S3_TARGET"
+    aws s3 cp --exclude "$OUTPUT/*.png" --recursive "$OUTPUT" s3://"$S3_TARGET"
   fi
 }
 
