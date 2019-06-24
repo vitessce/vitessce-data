9319cd5211551c2e23a33b4b796c2ff5d5317a4e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-06-24 10:30:30 -0400

    modify in-place; comment out debugging code

--- test.sh
+++ test.sh
@@ -30,16 +30,16 @@ CI=true ./linnarsson-osmfish.sh
 # CI to get it to run as test locally;
 # Redundant on Travis, but doesn't hurt anything.
 
-perl -pne 's/uuid:.{36}/uuid:00000000-0000-0000-0000-000000000000/;
+perl -pine \
+  's/uuid:.{36}/uuid:00000000-0000-0000-0000-000000000000/;
 	s/<AcquisitionDate>.{26}/<AcquisitionDate>0000-00-00T00:00:00.000000/;
-	s/tifffile.py.{20}/tifffile.py.0000:00:00 00:00:00/' fake-files/output/linnarsson.images.ome.tif > fake-files/output/result
+	s/tifffile.py.{20}/tifffile.py.0000:00:00 00:00:00/' fake-files/output/linnarsson.images.ome.tif
 
-mv -f fake-files/output/result fake-files/output/linnarsson.images.ome.tif
-
-hexdump -C fake-files/output/linnarsson.images.ome.tif > output-dump.txt
-hexdump -C fake-files/output-expected/linnarsson.images.ome.tif > output-expected-dump.txt
-
-diff -y output-dump.txt output-expected-dump.txt
+# If you have problems with the TIFF, try this:
+#
+# hexdump -C fake-files/output/linnarsson.images.ome.tif > output-dump.txt
+# hexdump -C fake-files/output-expected/linnarsson.images.ome.tif > output-expected-dump.txt
+# diff -y output-dump.txt output-expected-dump.txt
 
 diff -w -r fake-files/output fake-files/output-expected/ -x .DS_Store | head -n100 | cut -c 1-100
 end endtoend
