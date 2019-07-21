e13ef0a94f9f15fbaa64e35925a7dbf1b1022bc7
thomaslchan <thomaslchan@gmail.com>
2019-07-21 04:27:23 -0400

    renamed files

--- scripts/process_wang.sh
+++ scripts/process_wang.sh
@@ -11,52 +11,52 @@ main() {
 
     get_CLI_args "$@"
 
-    CSV_IN="$INPUT/mermaid.csv"
-    PNG_IN="$INPUT/mermaid.png"
+    CSV_IN="$INPUT/wang.csv"
+    PNG_IN="$INPUT/wang.png"
 
     CLI_ARGS="--csv_file $CSV_IN"
-    add_CLI_ARGS 'cells' 'mermaid'
-    add_CLI_ARGS 'molecules' 'mermaid'
-    add_CLI_ARGS 'images' 'mermaid'
+    add_CLI_ARGS 'cells' 'wang'
+    add_CLI_ARGS 'molecules' 'wang'
+    add_CLI_ARGS 'images' 'wang'
 
     echo "Download and process cells..."
 
     if [ ! -e "$CSV_IN" ]
     then
-        wget "$MERMAID_URL/data.csv.gz" -O "$CSV_IN.gz"
+        wget "$WANG_URL/data.csv.gz" -O "$CSV_IN.gz"
         gunzip -df "$CSV_IN"
     fi
 
     [ -e "$PNG_IN" ] || \
-        wget "$MERMAID_URL/bg.png" -O "$PNG_IN"
+        wget "$WANG_URL/bg.png" -O "$PNG_IN"
 
-    IMAGE_OUT="$OUTPUT/mermaid.png"
+    IMAGE_OUT="$OUTPUT/wang.png"
     if [ -e "$IMAGE_OUT" ]
     then
         echo "Skipping image -- output already exists: $IMAGE_OUT"
     else
-        cp $PNG_IN $OUTPUT/mermaid.png
+        cp $PNG_IN $OUTPUT/wang.png
     fi
 
-    TILE_BASE='mermaid.images'
+    TILE_BASE='wang.images'
     TILE_PATH="$OUTPUT/$TILE_BASE"
-    if [ -e "$OUTPUT/mermaid.images/info.json" ]
+    if [ -e "$OUTPUT/wang.images/info.json" ]
     then
         echo "Skipping tile -- output already exists: $TILE_PATH"
     else
         URL_PREFIX="https://s3.amazonaws.com/$S3_TARGET"
-        mkdir -p "$OUTPUT/mermaid.images/"
-        JSON_STRING='{ "type": "image", "url": "'$URL_PREFIX'/mermaid.png" }'
-        echo $JSON_STRING > "$OUTPUT/mermaid.images/info.json"
+        mkdir -p "$OUTPUT/wang.images/"
+        JSON_STRING='{ "type": "image", "url": "'$URL_PREFIX'/wang.png" }'
+        echo $JSON_STRING > "$OUTPUT/wang.images/info.json"
     fi
 
-    CELLS_OUT="$OUTPUT/mermaid.cells.json"
+    CELLS_OUT="$OUTPUT/wang.cells.json"
     if [ -e "$CELLS_OUT" ]
     then
         echo "Skipping cells -- output already exists: $CELLS_OUT"
     else
         echo 'Generating cells JSON may take a while...'
-        CMD="$BASE/python/mermaid_csv_reader.py $CLI_ARGS"
+        CMD="$BASE/python/wang_csv_reader.py $CLI_ARGS"
         echo "Running: $CMD"
         eval $CMD
     fi
@@ -64,7 +64,7 @@ main() {
 
 ### Globals
 
-MERMAID_URL='https://jef.works/MERmaid'
+WANG_URL='https://jef.works/MERmaid'
 
 ### Main
 
