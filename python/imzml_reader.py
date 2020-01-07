d3c94b27c9e4fb6344c1e5fd52e52c8a218212f3
Trevor Manz <trevor.j.manz@gmail.com>
2020-01-07 13:00:23 -0500

    convert IMS data to zarr format

--- python/imzml_reader.py
+++ python/imzml_reader.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import pandas as pd
-from pathlib import Path
 from pyimzml.ImzMLParser import ImzMLParser
 import zarr
 
@@ -8,11 +9,30 @@ import argparse
 
 
 class IMSDataset:
-    def __init__(self, imzml_file, ibd_file, micro_res=0.5, ims_res=10):
-        self.parser = ImzMLParser(filename=imzml_file, ibd_file=ibd_file)
+    """Converts IMS data stored as imzML into columnar or ndarray formats
+
+    :param imzml_file: path to `.imzML` file.
+    :param ibd_file: path to associated `.ibd` file.
+    :param micro_res: microscopy resolution in nm (used for scaling).
+    :param ims_res: IMS resolution in nm (used for scaling).
+    """
+
+    def __init__(self, imzml_file, ibd_file, micro_res, ims_res):
+        # When passing the ibd path explicitly,
+        # the file object must be opened manually
+        self.parser = ImzMLParser(
+            filename=imzml_file, ibd_file=open(ibd_file, "rb")
+        )
         self.micro_res = micro_res
         self.ims_res = ims_res
-        self.ims_px_in_micro = ims_res / micro_res
+        self.ims_px_in_micro = ims_res / micro_res  # scaling factor
+
+        mz_lengths = self.parser.mzLengths
+        if not (mz_lengths.count(mz_lengths[0]) == len(mz_lengths)):
+            raise ValueError(
+                "The number of m/z is not the same at each coordinate."
+            )
+        self.mzs, _ = self.parser.getspectrum(0)
 
     def __get_min_max_coords(self):
         coords = np.array(self.parser.coordinates)
@@ -20,40 +40,42 @@ class IMSDataset:
         x_max, y_max, _ = np.max(coords, axis=0)
         return x_min, y_min, x_max, y_max
 
-    def to_columnar(self, mz_precision=4, dtype="uint32"):
-        mzs, _ = self.parser.getspectrum(0)
-        coords = np.array(dataset.parser.coordinates)
+    def __format_mzs(self, precision=4):
+        return np.round(self.mzs, precision).astype(str)
+
+    def to_columnar(self, dtype="uint32"):
+        coords = np.array(self.parser.coordinates)
         x, y, _ = coords.T
 
         coords_df = pd.DataFrame(
             {
                 "x": x,
                 "y": y,
-                "micro_x_topleft": x * self.ims_px_in_micro - self.ims_px_in_micro,
-                "micro_y_topleft": y * self.ims_px_in_micro - self.ims_px_in_micro,
+                "micro_x_topleft": x * self.ims_px_in_micro
+                - self.ims_px_in_micro,
+                "micro_y_topleft": y * self.ims_px_in_micro
+                - self.ims_px_in_micro,
                 "micro_px_width": np.repeat(self.ims_px_in_micro, len(coords)),
             },
             dtype=dtype,
         )
 
-        intensities = np.zeros((len(coords_df), len(mzs)))
+        intensities = np.zeros((len(coords_df), len(self.mzs)))
         for i in range(len(coords)):
             _, coord_intensities = self.parser.getspectrum(i)
             intensities[i, :] = coord_intensities
 
         intensities = pd.DataFrame(
-            intensities, columns=np.round(mzs, mz_precision).astype(str), dtype=dtype
+            intensities, columns=self.__format_mzs(), dtype=dtype,
         )
 
         return coords_df.join(intensities)
 
     def to_array(self):
         x_min, y_min, x_max, y_max = self.__get_min_max_coords()
-        mz_lengths = self.parser.mzLengths
-        if not (mz_lengths.count(mz_lengths[0]) == len(mz_lengths)):
-            raise ValueError("The number of m/z is not the same at each coordinate.")
-
-        arr = np.zeros((x_max - x_min + 1, y_max - y_min + 1, mz_lengths[0]))
+        arr = np.zeros(
+            (x_max - x_min + 1, y_max - y_min + 1, self.parser.mzLengths[0])
+        )
 
         for idx, (x, y, _) in enumerate(self.parser.coordinates):
             _, intensities = self.parser.getspectrum(idx)
@@ -61,24 +83,43 @@ class IMSDataset:
 
         return arr
 
-    def write_zarr(self, path, dtype="i4"):
+    def write_zarr(self, path, dtype):
         arr = self.to_array()
         # zarr.js does not support compression yet
-        z_arr = zarr.open(path, mode="w", shape=arr.shape, compressor=None, dtype=dtype)
+        # https://github.com/gzuidhof/zarr.js/issues/1
+        z_arr = zarr.open(
+            path, mode="w", shape=arr.shape, compressor=None, dtype=dtype
+        )
+        # write array with metadata
         z_arr[:, :, :] = arr
+        x_min, y_min, x_max, y_max = self.__get_min_max_coords()
+        z_arr.attrs["x_extent"] = [float(x_min), float(x_max)]
+        z_arr.attrs["y_extent"] = [float(y_min), float(y_max)]
+        z_arr.attrs["scaling_factor"] = self.ims_px_in_micro
+        z_arr.attrs["mz"] = self.__format_mzs().tolist()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create zarr from Spraggins dataset.")
+    parser = argparse.ArgumentParser(
+        description="Create zarr from Spraggins dataset."
+    )
     parser.add_argument(
-        "--imzml_file", required=True, help="imzML file from Jeff Spraggins' lab.",
+        "--imzml_file",
+        required=True,
+        help="imzML file from Jeff Spraggins' lab.",
     )
     parser.add_argument(
         "--ibd_file",
         required=True,
-        help="Corresponding ibd file from Jeff Spraggin's lab",
+        help="Corresponding ibd file from Jeff Spraggins' lab",
+    )
+    parser.add_argument(
+        "--ims_file",
+        required=True,
+        help="Write the IMS data to this zarr file.",
     )
     args = parser.parse_args()
-
-    dataset = IMSDataset(parser.imzml_file, parser.ibd_file)
-    dataset.write_zarr(f"spraggins.zarr")
+    dataset = IMSDataset(
+        args.imzml_file, args.ibd_file, micro_res=0.5, ims_res=10
+    )
+    dataset.write_zarr(args.ims_file, dtype="i4")
--- scripts/process_spraggins.sh
+++ scripts/process_spraggins.sh
@@ -4,22 +4,18 @@ set -o errexit
 . ./scripts/utils.sh
 
 main() {
-    # Download and process data which describes cell locations, boundaries,
-    # and gene expression levels. Multiple JSON output files are produced:
-    # The files are redudant, but this reduces the processing that needs
-    # to be done on the client-side.
+    # Download and process data which describes an imaging 
+    # mass spectrometry (IMS) experiment. A single zarr store is created.
 
     get_CLI_args "$@"
+    IMZML_IN="$INPUT/spraggins.ims.imzml"
+    IBD_IN="$INPUT/spraggins.ims.ibd"
+    ZARR_OUT="$OUTPUT/spraggins.ims.zarr"
 
-    IMZML_IN="$INPUT/spraggins.imzml"
-    IBD_IN="$INPUT/spraggins.ibd"
-
-    CLI_ARGS="--imzml_file $IMZML_IN --ibd_file $IBD_IN"
-    add_CLI_ARGS 'ims' 'spraggins'
+    CLI_ARGS="--imzml_file $IMZML_IN --ibd_file $IBD_IN --ims_file $ZARR_OUT"
 
     echo "Download and process IMS data..."
 
-    # SPRAGGINS_URL "https://vitessce-data.s3.amazonaws.com/source-data/spraggins"
     SPRAGGINS_URL="http://localhost:8000"
 
     [ -e "$IMZML_IN" ] || \
@@ -27,13 +23,11 @@ main() {
     [ -e "$IBD_IN" ] || \
         wget "$SPRAGGINS_URL/VAN0001-RK-1-21-IMS.ibd" -O "$IBD_IN"
 
-    
-    ZARR_OUT="$OUTPUT/spraggins.zarr"
     if [ -e "$ZARR_OUT" ]
     then
         echo "Skipping zarr -- output already exists: $ZARR_OUT"
     else
-        echo 'Generating cells JSON may take a while...'
+        echo 'Generating IMS zarr may take a while...'
         CMD="$BASE/python/imzml_reader.py $CLI_ARGS"
         echo "Running: $CMD"
         eval $CMD
