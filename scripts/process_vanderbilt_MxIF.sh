1c3dde23b7472a91180cc6ff60c9b69c7f8bcc9a
ilan-gold <ilanbassgold@gmail.com>
2020-02-10 15:28:28 -0500

    Update tests and add Vanderbilt processing

---
+++ scripts/process_vanderbilt_MxIF.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -o errexit
+
+. ./scripts/utils.sh
+
+main() {
+
+  get_CLI_args "$@"
+
+  TILES_BASE='vanderbilt_MxIF.images'
+  TILES_PATH="$OUTPUT/$TILES_BASE"
+  if [ -e "$TILES_PATH" ]
+  then
+      echo "Skipping tiling -- output already exists: $TILES_PATH"
+  else
+      mkdir $TILES_PATH
+      SERVER_URL="https://vitessce-vanderbilt-data.storage.googleapis.com/"
+      CMD='docker run --rm
+          -e "SERVER_URL='$SERVER_URL'"
+          -e "PREFIX=vanderbilt_MxIF"
+          -e "PYRAMID_TYPE=tiff"
+          --mount "type=bind,src='$INPUT'/vanderbilt.ome.tif,destination=/input.ome.tif"
+          --mount "type=bind,src='$OUTPUT',destination=/output_dir"
+          --name tiler gehlenborglab/ome-tiff-tiler:v0.0.4'
+      echo "Running: $CMD"
+      eval $CMD
+  fi
+}
+
+main "$@"
