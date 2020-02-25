9f767032eb1f53ab9f235d58da283ee237c4e006
ilan-gold <ilanbassgold@gmail.com>
2020-02-25 11:24:59 -0500

    Update fixtures and output

--- scripts/process_vanderbilt.sh
+++ scripts/process_vanderbilt.sh
@@ -8,12 +8,11 @@ main() {
   get_CLI_args "$@"
 
   TILES_BASE='vanderbilt.images'
-  TILES_PATH="$OUTPUT/vanderbilt/$TILES_BASE"
+  TILES_PATH="$OUTPUT/$TILES_BASE"
   if [ -e "$TILES_PATH" ]
   then
       echo "Skipping tiling -- output already exists: $TILES_PATH"
   else
-      mkdir $TILES_PATH
       if [ -e "$INPUT/vanderbilt.ome.tif" ]
       then
         echo "Not copying $INPUT/vanderbilt.ome.tif from s3 - already exists or testing"
@@ -21,7 +20,7 @@ main() {
         aws s3 cp s3://vitessce-data/source-data/vanderbilt/vanderbilt.ome.tif "$INPUT/vanderbilt.ome.tif"
       fi
       RELEASE=${S3_TARGET//vitessce-data\//}
-      SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/"
+      SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/vanderbilt/"
       CMD='docker run --rm
           -e "SERVER_URL='$SERVER_URL'"
           -e "PREFIX=vanderbilt"
