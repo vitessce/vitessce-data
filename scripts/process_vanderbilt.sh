dd7bf73149b6b585165b22cd9488432fa95e1852
ilan-gold <43999641+ilan-gold@users.noreply.github.com>
2020-02-25 17:31:20 -0500

    Updaet schema outputs for vanderbilt (#74)

--- scripts/process_vanderbilt.sh
+++ scripts/process_vanderbilt.sh
@@ -13,7 +13,6 @@ main() {
   then
       echo "Skipping tiling -- output already exists: $TILES_PATH"
   else
-      mkdir $TILES_PATH
       if [ -e "$INPUT/vanderbilt.ome.tif" ]
       then
         echo "Not copying $INPUT/vanderbilt.ome.tif from s3 - already exists or testing"
@@ -21,18 +20,19 @@ main() {
         aws s3 cp s3://vitessce-data/source-data/vanderbilt/vanderbilt.ome.tif "$INPUT/vanderbilt.ome.tif"
       fi
       RELEASE=${CLOUD_TARGET//vitessce-data\//}
-      SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/"
+      SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/vanderbilt/"
       CMD='docker run --rm
           -e "SERVER_URL='$SERVER_URL'"
           -e "PREFIX=vanderbilt"
           -e "PYRAMID_TYPE=tiff"
           --mount "type=bind,src='$INPUT'/vanderbilt.ome.tif,destination=/input.ome.tif"
           --mount "type=bind,src='$OUTPUT',destination=/output_dir"
-          --name tiler gehlenborglab/ome-tiff-tiler:v0.0.4'
+          --name tiler gehlenborglab/ome-tiff-tiler:v0.0.5'
       echo "Running: $CMD"
       eval $CMD
       # vitessce relies on this naming strategy, whereas the docker image is more general
-      mv "$TILES_PATH/tiff.json" "$TILES_PATH/vanderbilt.raster.json"
+      mv "$TILES_PATH/tiff.json" "$OUTPUT/vanderbilt.raster.json" || \
+        sudo mv "$TILES_PATH/tiff.json" "$OUTPUT/vanderbilt.raster.json"
   fi
 }
 
