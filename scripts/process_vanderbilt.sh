969819702f389ff26f1d3ee0d953b47fcdc49304
ilan-gold <ilanbassgold@gmail.com>
2020-02-25 14:17:57 -0500

    Update permissions and revert cp

--- scripts/process_vanderbilt.sh
+++ scripts/process_vanderbilt.sh
@@ -31,9 +31,7 @@ main() {
       echo "Running: $CMD"
       eval $CMD
       # vitessce relies on this naming strategy, whereas the docker image is more general
-      ls -ld $TILES_PATH
-      ls -l $TILES_PATH
-      cp "$TILES_PATH/tiff.json" "$TILES_PATH/vanderbilt.raster.json"
+      mv "$TILES_PATH/tiff.json" "$TILES_PATH/vanderbilt.raster.json"
   fi
 }
 
