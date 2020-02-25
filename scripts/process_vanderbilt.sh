dfae6cd7a00e2efde5ba84a63151deffabf01ccb
ilan-gold <ilanbassgold@gmail.com>
2020-02-25 14:29:14 -0500

    Update json

--- scripts/process_vanderbilt.sh
+++ scripts/process_vanderbilt.sh
@@ -31,7 +31,6 @@ main() {
       echo "Running: $CMD"
       eval $CMD
       # vitessce relies on this naming strategy, whereas the docker image is more general
-      touch "$TILES_PATH/vanderbilt.raster.json"
       mv "$TILES_PATH/tiff.json" "$TILES_PATH/vanderbilt.raster.json"
   fi
 }
