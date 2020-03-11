403140486ea3c92c67cc6d8e4dcdd22dbf9c05cf
Trevor Manz <trevor.j.manz@gmail.com>
2020-03-11 16:16:23 -0400

    make sets complementary

--- process.sh
+++ process.sh
@@ -35,8 +35,10 @@ main() {
     then
         echo 'CI: Skip push to AWS and GCS'
     else
-        aws s3 cp --exclude "$OUTPUT/*.ome.tif*" --exclude "$OUTPUT/*.zarr/*" --recursive "$OUTPUT" s3://"$CLOUD_TARGET"
-        gsutil -m rsync -r -x ".*\.png$|.*\.json$" "$OUTPUT" gs://"$CLOUD_TARGET"
+        # All .json & .png extensions are synced to aws
+        # All non .json & .png extentions (i.e. .zarr / .ome.tiff) are pushed to google cloud storage
+        aws s3 cp --recursive --exclude "*" --include "*.png" --include "*.json" $OUTPUT s3://"$CLOUD_TARGET"
+        gsutil -m rsync -r -x ".*\.png$|.*\.json$" $OUTPUT gs://"$CLOUD_TARGET"
     fi
 }
 
