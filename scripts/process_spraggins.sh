78c6977c22107d86878aacd6b034d4ab1d35920f
Trevor Manz <trevor.j.manz@gmail.com>
2020-04-05 15:54:26 -0400

    Update bash scripts

--- scripts/process_spraggins.sh
+++ scripts/process_spraggins.sh
@@ -14,18 +14,21 @@ main() {
     IMS_IBD_IN="$INPUT/spraggins.ims.ibd"
     IMS_ZARR_OUT="$OUTPUT/spraggins.ims.zarr"
     IMS_JSON_OUT="$OUTPUT/spraggins.ims.raster.json"
+    IMS_RASTER_NAME="Spraggins IMS"
 
     # MXIF data
     MXIF_TIFF_IN="$INPUT/spraggins.mxif.ome.tif"
-    MXIF_TILES_OUT="$OUTPUT/spraggins.images"
+    MXIF_ZARR_OUT="$OUTPUT/spraggins.mxif.zarr"
     MXIF_JSON_OUT="$OUTPUT/spraggins.mxif.raster.json"
+    MXIF_RASTER_NAME="Spraggins MxIF"
+
 
     # CLOUD SOURCE
     SOURCE_URL="https://vitessce-data.s3.amazonaws.com/source-data/spraggins"
 
     # CLOUD TARGET
     RELEASE=${CLOUD_TARGET//vitessce-data\//}
-    SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/spraggins"
+    SERVER_URL="https://vitessce-data.storage.googleapis.com/$RELEASE/spraggins/"
 
     echo "Download and process IMS data..."
 
@@ -43,15 +46,16 @@ main() {
             --imzml_file $IMS_IMZML_IN
             --ibd_file $IMS_IBD_IN
             --ims_zarr $IMS_ZARR_OUT
-            --ims_metadata $IMS_JSON_OUT
-            --zarr_store_url $SERVER_URL"
+            --raster_json $IMS_JSON_OUT
+            --raster_name '$IMS_RASTER_NAME'
+            --server_url $SERVER_URL"
         echo "Running: $CMD"
         eval $CMD
     fi
 
-    if [ -e "$MXIF_TILES_OUT"]
+    if [ -e "$MXIF_ZARR_OUT" ]
     then
-        echo "Skipping tiling -- output already exists: $MXIF_TILES_OUT"
+        echo "Skipping tiling -- output already exists: $MXIF_ZARR_OUT"
     else
         if [ -e "$MXIF_TIFF_IN" ]
         then
@@ -59,18 +63,15 @@ main() {
         else
           wget "$SOURCE_URL/spraggins.ome.tif" -O "$MXIF_TIFF_IN"
         fi
-        CMD='docker run --rm
-            -e "SERVER_URL='$SERVER_URL'/"
-            -e "PREFIX=spraggins"
-            -e "PYRAMID_TYPE=tiff"
-            --mount "type=bind,src='$MXIF_TIFF_IN',destination=/input.ome.tif"
-            --mount "type=bind,src='$OUTPUT',destination=/output_dir"
-            --name tiler gehlenborglab/ome-tiff-tiler:v0.0.7'
+        echo 'Converting OME-TIFF to zarr may take a while...'
+        CMD="$BASE/python/ome_tiff_reader.py
+            --input_tiff $MXIF_TIFF_IN
+            --output_zarr $MXIF_ZARR_OUT
+            --raster_json $MXIF_JSON_OUT
+            --raster_name '$MXIF_RASTER_NAME'
+            --server_url $SERVER_URL"
         echo "Running: $CMD"
         eval $CMD
-        # vitessce relies on this naming strategy, whereas the docker image is more general
-        mv "$MXIF_TILES_OUT/tiff.json" "$MXIF_JSON_OUT" || \
-          sudo mv "$MXIF_TILES_OUT/tiff.json" "$MXIF_JSON_OUT"
     fi
 }
 
