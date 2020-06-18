c4d61a6afe1a6632817380e94a5003a2c0c5b614
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-17 21:46:12 -0400

    Comment

--- python/cell_reader.py
+++ python/cell_reader.py
@@ -241,7 +241,7 @@ def get_cell_sets(clusters, lookup):
         cluster_dict.update(subcluster)
 
     # Use the `hierarchy` dict to fill in an object
-    # conforming to the `cell-sets` schema.
+    # conforming to the `cell-sets.json` schema.
     cluster_nodes = []
     for cluster_name in sorted(hierarchy.keys()):
         cluster_dict = hierarchy[cluster_name]
